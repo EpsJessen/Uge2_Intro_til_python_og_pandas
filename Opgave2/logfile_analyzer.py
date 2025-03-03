0e4613ce343db8bdc004d2e12e5533b96cef5d47
Epsilon <epsijes@gmail.com>
2025-03-03 14:15:13 +0100

    Update logfile_analyzer.py
    Assumes that files are placed in Opgave2 directory

--- Opgave2/logfile_analyzer.py
+++ Opgave2/logfile_analyzer.py
@@ -2,9 +2,9 @@ import os.path
 
 log_types = ['SUCCESS', 'ERROR', 'WARNING', 'INFO']
 
-def open_logfile(file_path:str = "Opgave2/app_log.txt")->list[str]:
+def open_logfile(file_path:str = "app_log.txt")->list[str]:
     try:
-        path = os.path.join(file_path)
+        path = os.path.join("Opgave2", file_path)
         with open(path, "r") as log_file:
             logs = []
             for line in log_file:
