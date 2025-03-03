11607562b29036e31da4f6cb725b137042497d1a
Epsilon <epsijes@gmail.com>
2025-03-03 10:53:37 +0100

    Update nameData.py
    creating and printing dictionary of letter occurrences

--- Opgave1/nameData.py
+++ Opgave1/nameData.py
@@ -14,6 +14,21 @@ def sort_alphabetical(names:list[str])-> list[str]:
 def sort_length(names:list[str])->list[str]:
     return sorted(names, key=len)
     
+def dict_letters(names:list[str], case_difference:bool = False) -> dict[str, int]:
+    letters = {}
+    for name in names:
+        if not case_difference:
+            name = name.lower()
+        for letter in name:
+            letters[letter] = letters.get(letter, 0) + 1
+    return letters
+
+def print_letter_dict(letter_dict:dict[str, int]) -> None:
+    counts_list = letter_dict.items()
+    sorted_counts = sorted(counts_list)
+    for count in sorted_counts:
+        print(f"{count[0]} -> {count[1]}")
+
 def print_first_n(names:list[str], number:int) -> None:
     for i in range(min(number, len(names))):
         print(names[i])        
@@ -26,5 +41,10 @@ def main() -> None:
 
     len_sorted = sort_length(alph_sorted)
     print_first_n(len_sorted, 10)
+
+    name_dict = dict_letters(len_sorted, case_difference=False)
+    print_letter_dict(name_dict)
+
+
 if __name__ == "__main__":
     main()
