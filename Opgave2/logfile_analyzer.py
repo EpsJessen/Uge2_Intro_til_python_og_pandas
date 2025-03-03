5465b8e7c9d30f975753b73b0c668da983a42db5
Epsilon <epsijes@gmail.com>
2025-03-03 12:35:43 +0100

    Update logfile_analyzer.py
    use os.path

--- Opgave2/logfile_analyzer.py
+++ Opgave2/logfile_analyzer.py
@@ -1,6 +1,9 @@
+import os.path
+
 def open_logfile(file_path:str = "Opgave2/app_log.txt")->list[str]:
     try:
-        with open(file_path, "r") as log_file:
+        path = os.path.join(file_path)
+        with open(path, "r") as log_file:
             logs = []
             for line in log_file:
                 logs.append(line.strip())
