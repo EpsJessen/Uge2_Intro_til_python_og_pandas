f39c070b141ccb2cfabc59d17b86013f1f6129fd
Epsilon <epsijes@gmail.com>
2025-03-07 09:53:00 +0100

    Update python_and_sql.py
    Calculate price and extract + plot mean spending by country

--- Opgave5/python_and_sql.py
+++ Opgave5/python_and_sql.py
@@ -24,5 +24,13 @@ with engine.connect() as connection:
     query += "inner join orderdetails on orders.orderid = orderdetails.orderid "
     query += "inner join products on orderdetails.productid = products.productid;"
     nw_full_orders = pd.read_sql(query, connection)
-    print(nw_full_orders.head(10))
+    #print(nw_full_orders.head(10))
 
+    #Add column for the total price of an order
+    nw_full_orders["totalprice"] = nw_full_orders.unitprice * nw_full_orders.quantity * (1-nw_full_orders.discount)
+    
+    ship_country_group = nw_full_orders.groupby(["shipcountry"])
+    mean_price_by_country = ship_country_group[["totalprice"]].mean()
+    mean_price_by_country.plot(kind = "bar", legend=False, title="Mean spending per order by country")
+    plt.tight_layout()
+    plt.show()
